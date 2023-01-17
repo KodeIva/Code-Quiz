52f21d1f8ed7a4e0268d5c3f952c9a061a9b9395
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-01-17 21:23:32 +0000

    Add code update

--- assets/logic.js
+++ assets/logic.js
@@ -22,15 +22,7 @@ let questions = [
 ]
 
 let currentQuestion = 0
-/*
-for(let i = 0; i < questions.length;i++){
-  console.log(questions[i].question);
-  currentQuestion = questions[i].question
-  console.log(currentQuestion);
-}
-*/
-
-let time = 20
+let time = 60
 let winningPoints = 0
 let startBtn = document.getElementById('start')
 let questionDiv = document.getElementById('questions')
@@ -41,6 +33,7 @@ let subBtn = document.getElementById('submit')
 let input = document.getElementById('initials')
 let startScreen = document.getElementById('start-screen')
 
+
 // Setting the timer
 function setTimer() {
   let interval  = setInterval(function() {
@@ -50,10 +43,7 @@ function setTimer() {
 
   if(time == 0 || time < 0) {
    clearInterval(interval)
-   questionDiv.classList.add('hide')
-   questionTitle.classList.add('hide')
-   endScreen.classList.remove('hide')
-   finalScore.textContent = winningPoints
+   finishQuiz()
   }
  },1000)
 }
@@ -67,38 +57,61 @@ startBtn.addEventListener('click', function() {
 
 // Generating the questions and answers
 function generateQuestions() {
+  let correctWrong = document.getElementById('correct-wrong')
   let choices = document.getElementById('choices')
+  let ol = document.createElement('ol')
+  let h1 = document.createElement('h1')
+  
+  choices.innerHTML = ''
+  
+  choices.appendChild(ol) 
   questionDiv.classList.remove('hide')
   questionTitle.classList.remove('hide')
-  questionTitle.textContent = questions[currentQuestion].question
   
-  let ol = document.createElement('ol')
-  choices.append(ol)
+  questionTitle.textContent = questions[currentQuestion].question
 
+   
+ console.log(currentQuestion);
  for(let i = 0; i < questions[currentQuestion].answers.length; i++) {
    let li = document.createElement('li')
+   ol.append(li)
    let btn = document.createElement('button')
    btn.textContent = questions[currentQuestion].answers[i]
-   ol.appendChild(li) 
-   li.appendChild(btn)
-   
+   li.append(btn)
+
    btn.addEventListener('click', function(e) {
     if(e.target.innerHTML !== questions[currentQuestion].correctAnswer) {
       time = time - 10
-      let h1 = document.createElement('h1')
-      h1.textContent = `Wrong!`
+      h1.textContent = 'Wrong!' 
+      choices.appendChild(h1)
       h1.setAttribute('style', 'color: #ef626c; font-style: italic');
-      questionDiv.appendChild(h1)
+     
     }else{
       winningPoints += 10
-      let h1 = document.createElement('h1')
-      h1.textContent = `Correct!`
+      h1.textContent = 'Correct!'
+      choices.appendChild(h1)
       h1.setAttribute('style', 'color: #00ad93; font-style: italic')
-      questionDiv.appendChild(h1)
+    }
+   
+     currentQuestion++
+
+    if(currentQuestion < questions.length) {
+      setTimeout( generateQuestions, 1000) 
+    }else{
+      console.log('Finish');
+      setTimeout(finishQuiz,1000)
     }
    })
  }
 }
+
+function finishQuiz() {
+  questionDiv.classList.add('hide')
+  questionTitle.classList.add('hide')
+  endScreen.classList.remove('hide')
+  finalScore.textContent = winningPoints
+}
+
 
 // Adding functionality to add user initials and points
 subBtn.addEventListener('click', (e) => {
